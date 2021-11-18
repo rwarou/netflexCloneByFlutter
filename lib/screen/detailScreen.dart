0d005f84285f584e0c0e4f624723731467294073
rwarou <rwarou.yim@gmail.com>
2021-11-19 08:58:55 +0900

    상세 페이지

---
+++ lib/screen/detailScreen.dart
@@ -0,0 +1,119 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:ncbf/model/movie.dart';
+
+class DetailScreen extends StatefulWidget {
+  final Movie movie;
+  DetailScreen({this.movie});
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  bool like = false;
+  @override
+  void initState() {
+    super.initState();
+    like = widget.movie.like;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: SafeArea(
+          child: ListView(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('images/' + widget.movie.poster),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: ClipRect(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Container(
+                          alignment: Alignment.center,
+                          color: Colors.black.withOpacity(0.1),
+                          child: Container(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.fromLTRB(0, 45, 0, 10),
+                                  child: Image.asset(
+                                      'images/' + widget.movie.poster),
+                                  height: 300,
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(7),
+                                  child: Text(
+                                    '99% 일치 2019 15+ 시즌 1개',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 13),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(7),
+                                  child: Text(
+                                    widget.movie.title,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(3),
+                                  child: FlatButton(
+                                    onPressed: () {},
+                                    color: Colors.red,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(Icons.play_arrow),
+                                        Text('PLAY'),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(5),
+                                  child: Text(
+                                    widget.movie.toString(),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(5),
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    '출연: 현빈, 손예진, 서지혜\n제작자: 이정효, 박지은',
+                                    style: TextStyle(
+                                        color: Colors.white60, fontSize: 12),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                      child: AppBar(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                  )),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
