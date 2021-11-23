31c8c21ea7589766e856c722639ec1e45d887fd4
rwarou <rwarou.yim@gmail.com>
2021-11-23 19:55:43 +0900

    2021 ver. connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ncbf/screen/homeScreen.dart';
 import 'package:ncbf/screen/moreScreen.dart';
 import 'package:ncbf/widget/bottomBar.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -11,11 +15,33 @@ class MyApp extends StatefulWidget {
     return _MyAppState();
   }
 
-  // _MyAppState createSate() => _MyAppState();
+  _MyAppState createSate() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   TabController controller;
+
+  bool _initialized = false;
+  bool _error = false;
+
+  void initializeFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (error) {
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
