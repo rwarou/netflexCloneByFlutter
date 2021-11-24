5ef82692ccc453647fdb702d381bd395ae2df64f
rwarou <rwarou.yim@gmail.com>
2021-11-24 09:05:19 +0900

    search bar 제작중

--- lib/screen/searchScreen.dart
+++ lib/screen/searchScreen.dart
@@ -23,11 +23,77 @@ class _SearchScreenState extends State<SearchScreen> {
     return Container(
       child: Column(
         children: <Widget>[
+          Padding(padding: EdgeInsets.all(30)),
           Container(
             color: Colors.black,
             padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
             child: Row(
-              children: <Widget>[],
+              children: <Widget>[
+                Expanded(
+                  flex: 6,
+                  child: TextField(
+                    focusNode: focusNode,
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                    autofocus: true,
+                    controller: _filter,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white12,
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.white60,
+                        size: 20,
+                      ),
+                      suffixIcon: focusNode.hasFocus
+                          ? IconButton(
+                              icon: Icon(
+                                Icons.cancel,
+                                size: 20,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _filter.clear();
+                                  _searchText = "";
+                                });
+                              },
+                            )
+                          : Container(),
+                      hintText: 'SEARCH',
+                      labelStyle: TextStyle(color: Colors.white),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                focusNode.hasFocus
+                    ? Expanded(
+                        child: FlatButton(
+                          child: Text('취소'),
+                        ),
+                      )
+                    : Expanded(
+                        flex: 0,
+                        child: Container(),
+                      )
+              ],
             ),
           )
         ],
